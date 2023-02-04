2f2c05b692fa5b75d1cf59b815d99128292ba354
Winspear22 <adnen.aloui2@hotmail.com>
2023-02-04 21:48:04 +0100

    ft_containers -- creation du template vector -- FONCTIONNEL/NOLEAK

--- srcs/test.cpp
+++ srcs/test.cpp
@@ -2,6 +2,18 @@
 
 int main( void )
 {
-    std::cout << "J'ai bien compile" << std::endl;
+	ft::vector<std::string> vector;
+
+	vector.SetTab("salut mon gars");
+
+	ft::vector<std::string> vector2(vector);
+	ft::vector<std::string> vector3 = vector2;
+
+	std::cout << "vector == " << vector.GetTab() << std::endl;
+	std::cout << "vector2 == " << vector2.GetTab() << std::endl;
+	std::cout << "vector3 == " << vector3.GetTab() << std::endl;
+
+
+
     return (0);
 }
--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -6,20 +6,55 @@
 /*   By: adaloui <adaloui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 20:04:27 by adaloui           #+#    #+#             */
-/*   Updated: 2023/02/04 20:23:55 by adaloui          ###   ########.fr       */
+/*   Updated: 2023/02/04 21:43:39 by adaloui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-# include <iomanip>
-# include <iostream>
+# include "utils.hpp"
 
-/*template<typename T>
-class vector
+namespace ft
 {
-	
-};*/
+	template<typename T>
+	class vector
+	{
+		public:
+			vector<T>( void )
+			{
+				std::cout << BGREEN << "Vector container constructor called" << NORMAL << std::endl;
+				return ;
+			}
+			vector<T>( vector<T> const & copy )
+			{
+				std::cout << BCYAN << "Vector container copy constructor called" << NORMAL << std::endl;
+				*this = copy;
+				//return (this);
+			}
+			~vector<T>( void )
+			{
+				std::cout << BRED << "Vector container destructor called." << NORMAL << std::endl;
+				return ;	
+			}
+			vector<T> & operator=( const vector<T> & rhs )
+			{
+				std::cout << BPURPLE << "Vector container destructor called." << NORMAL << std::endl;
+				this->_tab = rhs._tab;
+				return (*this);
+			}
+			void	SetTab( T tab )
+			{
+				this->_tab = tab;
+			}
+			T 		GetTab( void )
+			{
+				return (this->_tab);
+			}
+		private:
+			T _tab;
 
+	};
+
+}
 #endif
